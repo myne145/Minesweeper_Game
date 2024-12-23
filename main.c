5c5b8880bab7be7c14428e43456da883bae2905c
antekk <antoni.kucz@gmail.com>
2024-12-23 21:45:34 +0100

    removed debug while loop in main.c

--- main.c
+++ main.c
@@ -27,12 +27,8 @@ int main(int argc, char** argv) {
                 sscanf(optarg, "%zux%zux%zu", &rows, &cols, &bombs);
                 printf("Making game with size %zux%zu and %zu bombs.\n", rows, cols, bombs);
                 srand(time(NULL));
-                while (1)
-                    {
-                        board* board = make_board(rows, cols, bombs);
-                        start_game_from_board(board);
-                    }
-
+                board* board = make_board(rows, cols, bombs);
+                start_game_from_board(board);
                 break;
             }
             case 'h':{
