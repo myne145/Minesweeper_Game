5ade99ba49718410c124613e9348059a62fc15a8
antoni.kucz@gmail.com <antoni.kucz@gmail.com>
2025-01-11 21:35:34 +0100

    main.c rework part 1

--- src/Board/board.h
+++ src/Board/board.h
@@ -56,6 +56,6 @@ void randomize_solution_to_board(board* board,size_t firstRow, size_t firstCol);
  * @param preset preset gry - jeśli niestandardowy podaj NULL
  * @param gameBoard plansza
  */
-void print_board_stats(size_t seed, char* preset, board* gameBoard);
+void print_board_stats(size_t seed, board* gameBoard);
 
 #endif  // BOARD_H
