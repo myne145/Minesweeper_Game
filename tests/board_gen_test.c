d65d1885f29a20882f477834d6153468060b833d
antoni.kucz@gmail.com <antoni.kucz@gmail.com>
2025-01-23 16:28:21 +0100

    added loading board from file test

--- tests/board_gen_test.c
+++ tests/board_gen_test.c
@@ -2,6 +2,7 @@
 // Created by antekk on 1/23/25.
 //
 #include "src/Board/board.h"
+#include "src/SaveAndLoadGame/save_load.h"
 
 
 int main(int argc, char** argv) {
@@ -23,4 +24,14 @@ int main(int argc, char** argv) {
 
     //Free board test
     free_board(gameBoard);
+
+
+    //Loading board from file test
+    board* loadedBoard = load_game("test_game.bin");
+    board_assert(loadedBoard);
+    print_board_game(loadedBoard);
+
+    reveal_all_bombs(loadedBoard);
+    print_board_game(loadedBoard);
+
 }
